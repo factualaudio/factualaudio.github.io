132945114043822267e6047a07d45c58e026dbb3
Etienne Dechamps <etienne@edechamps.fr>
2017-11-21 21:11:47 +0000

    Publishing from source repository commit 06a970c3f0c1b528205301873c5dd8a802925dc5
    Hugo version: Hugo Static Site Generator v0.25.1 linux/amd64 BuildDate: 2017-08-03T09:06:42+01:00
    
    Octave version: GNU Octave, version 4.2.1

---
+++ inline_footnotes.js
@@ -0,0 +1,33 @@
+// Converts inline footnotes to traditional footnotes that bigfoot.js can recognize.
+$(function() {
+	footnotes = $("<ol/>").appendTo($("<div/>", {"class": "footnotes"}).appendTo($("article")));
+
+	var inline_footnotes = $(".inlineFootnote");
+
+	// We process the footnotes in two passes so that nested footnotes are handled correctly.
+
+	// First pass: add footnote link to each inline footnote and allocate a slot in the footnote list.
+	inline_footnotes.each(function() {
+		var index = footnotes.children("li").length + 1;
+		var id = "inline:" + index;
+
+		var bigfootnote = $("<span/>", {"class": $(this).attr("class")}).append(
+			$("<sup/>", {"id": "fnref:" + id}).append(
+				$("<a/>", {"href": "#fn:" + id, rel: "footnote", text: index})));
+		$(this).after(bigfootnote);
+
+		var item = $("<li/>", {"id": "fn:" + id}).append(
+			$("<a/>", {"href": "#fnref:" + id, "title": "return to article", text: " ↩"}));
+		footnotes.append(item);
+		$(this).data("inlineFootnoteItem", item);
+	});
+
+	// Second pass: move the contents of the inline footnotes into the list.
+	// We process the list in reverse so that innermost footnotes are processed first.
+	$(inline_footnotes.get().reverse()).each(function() {
+		$(this).data("inlineFootnoteItem").prepend($(this).contents());
+		$(this).remove();
+	});
+
+	$('.inlineFootnoteDecoration').remove();
+});
